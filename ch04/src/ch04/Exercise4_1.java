9e777e5e387c486508a8443dea1055a5948e3832
KOITT <KOITT@KOITT-PC>
2018-06-05 21:48:01 +0900

    4장 끝까지 완료 및 연습문제

---
+++ ch04/src/ch04/Exercise4_1.java
@@ -0,0 +1,12 @@
+package ch04;
+
+public class Exercise4_1 {
+	public static void main(String[] args) {
+		int year = 0;
+		char ch = 2;
+		boolean powerOn = false;
+		String str = "yes";
+		boolean i = str == "yes";
+		System.out.println(i);
+	}
+}
---
+++ ch04/src/ch04/FlowEx28.java
@@ -0,0 +1,24 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class FlowEx28 {
+	public static void main(String[] args) {
+		int input = 0, answer = 0;
+
+		answer = (int) (Math.random() * 100) + 1; // ~100사이의 임의의 수를 저장
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			System.out.print("1과 100사이의 정수를 입력하세요.>");
+			input = sc.nextInt();
+
+			if (input > answer) {
+				System.out.println("더 작은 수로 다시 시도해 보세요.");
+			} else if (input < answer) {
+				System.out.println("더 큰수로 다시 시도해 보세요.");
+			}
+		} while (input != answer);
+		System.out.println("정답입니다.");
+	}
+}
